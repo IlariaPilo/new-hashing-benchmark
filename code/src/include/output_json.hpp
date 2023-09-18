349afd81569858d80e7e5e016aa1d401c26bce46
Ilaria Pilo <ilaria.pilo@studenti.polito.it>
2023-09-18 09:38:17 +0200

    Complete output utility

--- code/src/include/output_json.hpp
+++ code/src/include/output_json.hpp
@@ -4,21 +4,40 @@
 
 #include <iostream>
 #include <fstream>
+#include <ctime>
+#include <unistd.h>
+#include <limits.h>
+#include <sys/sysinfo.h>
+#include <chrono>
+#include <iomanip>
+#include <sstream>
+
 #include <nlohmann/json.hpp>
+
 
 using json = nlohmann::json;
 
 class JsonOutput {
 public:
-    JsonOutput(const std::string& filename) : filename(filename) {
+    JsonOutput(const std::string& file_directory, const std::string& arg0, const std::string& filter = "") {
+        // first, get current time
+        std::time_t current_time = std::time(nullptr);
+        std::tm* local_time = std::localtime(&current_time);
+        // Format the date and time as "YYYY-MM-DD-HH-MM"
+        std::stringstream ss;
+        ss << std::put_time(local_time, "%Y-%m-%d-%H-%M");
+        // define filename
+        std::string filename = file_directory + "/" + ss.str() + filter + "_results.json";
+
         // Open the JSON file for writing
         output_file.open(filename);
         if (!output_file.is_open()) {
             std::cerr << "Error opening JSON file: " << filename << std::endl;
         } else {
-            // Start the JSON object
-            // TODO - create the thing nicely with the time and other things
-            json_output["data"] = json::array();
+            // First, we make the context
+            json_output["context"] = make_context(arg0, local_time);
+            // Then, we create the benchmark array
+            json_output["benchmarks"] = json::array();
         }
     }
 
@@ -32,12 +51,32 @@ public:
 
     void add_data(const json& obj) {
         // Add a JSON object to the array
-        // TODO - create the thing nicely with the time and other things
-        json_output["data"].push_back(obj);
+        json_output["benchmarks"].push_back(obj);
     }
 
 private:
-    std::string filename;
     json json_output;
     std::ofstream output_file;
+
+    json make_context(const std::string& arg0, std::tm* local_time) {
+        json context;
+        // -------------- date -------------- //
+        // Get the time zone offset in minutes
+        int time_zone = local_time->tm_gmtoff / 60;
+        std::stringstream format_time;
+        format_time << std::put_time(local_time, "%Y-%m-%dT%H:%M:%S");
+        // Format the time zone offset
+        format_time << std::showpos << std::internal << std::setw(3) << std::setfill('0')
+                    << time_zone / 60 << ":" << std::noshowpos
+                    << std::setw(2) << std::setfill('0') << time_zone % 60;
+        context["date"] = format_time.str();
+
+        char hostname[HOST_NAME_MAX];
+        gethostname(hostname, HOST_NAME_MAX);
+        context["host_name"] = hostname;
+        context["executable"] = arg0;
+        context["num_cpus"] = sysconf(_SC_NPROCESSORS_ONLN);
+        //context["mhz_per_cpu"] = TODO, maybe;
+        return context;
+    }
 };
