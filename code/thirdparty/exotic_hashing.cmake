454e8ef294a9e7ef2a61e7bfccf1e1b1428227cd
Ilaria Pilo <ilaria.pilo@studenti.polito.it>
2023-09-20 08:45:30 +0200

    Change to fork

--- code/thirdparty/exotic_hashing.cmake
+++ code/thirdparty/exotic_hashing.cmake
@@ -7,7 +7,7 @@ include(FetchContent)
 set(EXOTIC_HASHING_LIBRARY exotic-hashing)
 FetchContent_Declare(
   ${EXOTIC_HASHING_LIBRARY}
-  GIT_REPOSITORY https://github.com/DominikHorn/exotic-hashing.git
-  GIT_TAG d835390 
+  GIT_REPOSITORY https://github.com/IlariaPilo/exotic-hashing.git
+  GIT_TAG d08bdd3 
   )
 FetchContent_MakeAvailable(${EXOTIC_HASHING_LIBRARY})
