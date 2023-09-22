7ce0f7637a927a2f37fb1fcd83724a284cb3b7f3
Ilaria Pilo <ilaria.pilo@studenti.polito.it>
2023-09-22 12:31:30 +0200

    Add gaps results

--- scripts/print_figures.py
+++ scripts/print_figures.py
@@ -2,6 +2,7 @@ import json
 import sys
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 import os
 import re
 from math import log10
@@ -29,7 +30,7 @@ prefix = BASE_DIR + 'figs/' + prefix
 # ===================================================================== #
 
 # ------- function names ------- #
-SHAPES = {
+SHAPES_FN = {
     'RMI': 's',
     'PGM': 's',
     'Murmur': 'D',
@@ -41,6 +42,14 @@ SHAPES = {
     'XXHash': 'D',
     'MultiplyPrime': 'D',
     'FibonacciPrime': 'D'
+}
+SHAPES_DS = {
+    'gap_10': '^',
+    'uniform': '<',
+    'fb': 's',
+    'osm': 'D',
+    'wiki': 'o',
+    'normal': '>'
 }
 COLORS = {
     'RMI': '#a50026',
@@ -88,26 +97,34 @@ def get_rmi_models(label):
 
 # ------- collision plot ------- #
 def collisions(df):
+    datasets = ['gap_10','uniform','normal','wiki','fb']
     df = df[df["label"].str.lower().str.contains("collision")].copy(deep=True)
+    if df.empty:
+        return
+    df = df.sort_values(by='throughput_M')
     # Group the DataFrame by the 'dataset_name' column
     g_ds = df.groupby('dataset_name')
     
     # Create a single figure with multiple subplots in a row
-    num_subplots = len(g_ds)
-    fig, axes = plt.subplots(1, num_subplots, figsize=(12, 2))  # Adjust figsize as needed
+    num_subplots = len(datasets)
+    fig, axes = plt.subplots(1, num_subplots, figsize=(23, 2.5))  # Adjust figsize as needed
     
     # Create a single legend for all subplots
     legend_labels = []
     
     # Iterate through the groups and create subplots
-    for i, (name_ds, group_ds) in enumerate(g_ds):
+    i = 0
+    for name_ds, group_ds in g_ds:
+        if name_ds not in datasets:
+            continue
         g_fn = group_ds.groupby('function')
         ax = axes[i]
+        i += 1
         
         # Plot the data for the current group
         for name_fs, group_fn in g_fn:
             # Plot the data and collect labels for the legend
-            ax.plot(group_fn['throughput_M'], group_fn['collisions']/group_fn['data_elem_count'], color=COLORS[name_fs], marker=SHAPES[name_fs], label=name_fs)
+            ax.plot(group_fn['throughput_M'], group_fn['collisions']/group_fn['data_elem_count'], color=COLORS[name_fs], marker=SHAPES_FN[name_fs], label=name_fs)
             if name_fs not in legend_labels:
                 legend_labels.append(name_fs)
         
@@ -116,7 +133,7 @@ def collisions(df):
         # Format the y-axis to display only two digits after the decimal point
         ax.yaxis.set_major_formatter(plt.FormatStrFormatter('%.2f'))
         ax.set_ylim([0, 1])
-        ax.set_xlim([0, 150])
+        #ax.set_xlim([0, 150])
         ax.grid(True)
     
     # Add a single legend to the entire figure with labels on the same line
@@ -126,13 +143,17 @@ def collisions(df):
     labx = fig.supxlabel('Hash Computation Throughput (Million operations/s)')
     laby = fig.supylabel('Ratio of Colliding Keys')
 
+    #plt.show()
     fig.savefig(f'{prefix}_collisions.png', bbox_extra_artists=(lgd,labx,laby,), bbox_inches='tight')
 
 # ------- collision RMI plot ------- #
 def collisions_rmi(df):
     df = df[df["label"].str.lower().str.contains("collision")].copy(deep=True)
     df = df[df["function"] == "RMI"]
+    if df.empty:
+        return
     df["models"] = df["label"].apply(lambda x : get_rmi_models(x))
+    df = df.sort_values(by='models')
 
     uniform = df[df["dataset_name"] == "uniform"]
     normal = df[df["dataset_name"] == "normal"]
@@ -144,8 +165,8 @@ def collisions_rmi(df):
 
     # axes[0].set_xscale('log')
     # axes[1].set_xscale('log')
-    axes[0].plot(uni_ticks, uniform['collisions']/uniform['data_elem_count'], color=COLORS['RMI'], marker=SHAPES['RMI'])
-    axes[1].plot(normal_ticks, normal['collisions']/normal['data_elem_count'], color=COLORS['RMI'], marker=SHAPES['RMI'])        
+    axes[0].plot(uni_ticks, uniform['collisions']/uniform['data_elem_count'], color=COLORS['RMI'], marker=SHAPES_FN['RMI'])
+    axes[1].plot(normal_ticks, normal['collisions']/normal['data_elem_count'], color=COLORS['RMI'], marker=SHAPES_FN['RMI'])        
 
     # ticks
     uni_tick_labels = [f"$10^{int(tick)}$" for tick in uni_ticks]
@@ -168,6 +189,30 @@ def collisions_rmi(df):
 
     fig.savefig(f'{prefix}_collisions_RMI.png', bbox_extra_artists=(labx,laby,), bbox_inches='tight')
 
+# ------- gaps plot ------- #
+def gaps(df):
+    df = df[df["label"].str.lower().str.contains("gaps")].copy(deep=True)
+    if df.empty:
+        return
+    fig = plt.figure(figsize=(5, 3.5))
+    datasets = df['dataset_name'].unique()
+    for _d_ in datasets:
+        if _d_ == 'seq':
+            continue
+        _df_ = df[df['dataset_name'] == _d_]
+        # Assert that the DataFrame has only one row
+        assert _df_.shape[0] == 1
+        y = _df_['count'].values[0]
+        y = y[:min(len(y)+1,6)]
+        x = np.arange(min(len(y),6))
+        plt.plot(x, y, marker=SHAPES_DS[_d_], markersize=4, label=_d_) # MARKER TODO
+
+    plt.xlabel('Gap value')
+    plt.ylabel('Count')
+    plt.grid(True)
+    fig.legend(loc='center right') #, bbox_to_anchor=(0.5, 1.33))
+    fig.savefig(f'{prefix}_gaps.png')
+
 
 # =============================== MAIN =============================== #
 
@@ -179,5 +224,8 @@ with open(file_path, 'r') as json_file:
     # compute function names
     df["function"] = df["label"].apply(lambda x : get_fn_name(x))
     # compute throughput
-    df['throughput_M'] = df.apply(lambda x : x['data_elem_count']/(x['tot_time_s']*10**6), axis=1)
+    if 'tot_time_s' in df.columns:
+        df['throughput_M'] = df.apply(lambda x : x['data_elem_count']/(x['tot_time_s']*10**6), axis=1)
+    collisions(df)
     collisions_rmi(df)
+    gaps(df)
