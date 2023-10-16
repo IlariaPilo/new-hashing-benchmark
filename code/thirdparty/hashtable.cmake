4899be71e8c2889a21b2aeeb1f4e9b8b12fa14d3
Ilaria Pilo <ilaria.pilo@studenti.polito.it>
2023-10-16 16:23:50 +0200

    sync hashtable dependency

--- code/thirdparty/hashtable.cmake
+++ code/thirdparty/hashtable.cmake
@@ -8,6 +8,6 @@ set(HASHTABLE_LIBRARY hashtable)
 FetchContent_Declare(
   ${HASHTABLE_LIBRARY}
   GIT_REPOSITORY https://github.com/IlariaPilo/hashtable
-  GIT_TAG 72d7569
+  GIT_TAG 219505f
   )
 FetchContent_MakeAvailable(${HASHTABLE_LIBRARY})
