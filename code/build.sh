271b4caa358bce879239a31d8a242f8f154ca590
Ilaria Pilo <ilaria.pilo@studenti.polito.it>
2023-10-17 18:13:04 +0200

    Make build file more secure

--- code/build.sh
+++ code/build.sh
@@ -14,6 +14,17 @@ TARGET=${1:-"benchmarks perf_bm"}
 BUILD_TYPE=${2:-"RELEASE"}
 BUILD_DIR="cmake-build-$(echo "${BUILD_TYPE}" | awk '{print tolower($0)}')/"
 
+# check in which branch are we
+if [ -d "${BUILD_DIR}" ] && [ -e "${BUILD_DIR}/_deps/hashtable-src/include/thirdparty/spinlock.hpp" ]; then
+    read -p "It looks like you built the multiT branch. Do you want to remove it and build the singleT one? [y/N] " remove_choice
+    if [ "$remove_choice" = "y" ] || [ "$remove_choice" = "Y" ]; then
+        rm -fr ${BUILD_DIR}
+    else
+        echo "Operation aborted. No files removed."
+        exit 1
+    fi
+fi
+
 # Generate cmake project files
 cmake \
   -D CMAKE_BUILD_TYPE=${BUILD_TYPE} \
